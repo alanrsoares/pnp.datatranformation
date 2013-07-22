78c50dae388980c6ef60168e6e4351f0fd1dc3fa
Alan R. Soares <alanrsoars@gmail.com>
2013-07-22 16:13:29 -0300

    Creates mock data generator

--- src/Vtex.Practices.DataTransformation.Tests/Tests.cs
+++ src/Vtex.Practices.DataTransformation.Tests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using NPOI.SS.UserModel;
 using NUnit.Framework;
 using Newtonsoft.Json;
@@ -33,9 +34,10 @@ namespace Vtex.Practices.DataTransformation.Tests
             var columnMapper = AutoMapper();
 
             var handler = new DataHandler<DummyDto>(columnMapper);
+
             var workBook = handler.EncodeDataToWorkbook(Data);
 
-            handler.WriteToFile(@"C:\Temp\TestAutomap.xls", workBook);
+            Assert.DoesNotThrow(() => handler.WriteToFile(@"C:\Temp\TestAutomap.xls", workBook));
         }
 
         [Test]
@@ -46,7 +48,7 @@ namespace Vtex.Practices.DataTransformation.Tests
             var handler = new DataHandler<DummyDto>(columnMapper);
             var workBook = handler.EncodeDataToWorkbook(Data);
 
-            handler.WriteToFile(@"C:\Temp\TestManualmap.xls", workBook);
+            Assert.DoesNotThrow(() => handler.WriteToFile(@"C:\Temp\TestManualmap.xls", workBook));
         }
 
         [Test]
@@ -55,6 +57,7 @@ namespace Vtex.Practices.DataTransformation.Tests
             const string filePath = @"C:\Temp\TestAutomap.xls";
 
             var mapper = AutoMapper();
+
             var handler = new DataHandler<DummyDto>(mapper);
 
             var result = handler.DecodeFileToDtoCollection(filePath);
@@ -71,7 +74,9 @@ namespace Vtex.Practices.DataTransformation.Tests
 
             var handler = new DataHandler<DummyDto>(mapper);
 
-            var workBook = handler.EncodeDataToWorkbook(Data);
+            var expectedData = Data;
+
+            var workBook = handler.EncodeDataToWorkbook(expectedData);
 
             handler.WriteToFile(filePath, workBook);
 
@@ -79,7 +84,7 @@ namespace Vtex.Practices.DataTransformation.Tests
 
             var serializedResult = JsonConvert.SerializeObject(result);
 
-            var serializedData = JsonConvert.SerializeObject(Data);
+            var serializedData = JsonConvert.SerializeObject(expectedData);
 
             Assert.AreEqual(serializedResult, serializedData);
         }
@@ -113,36 +118,14 @@ namespace Vtex.Practices.DataTransformation.Tests
         {
             get
             {
-                return new List<DummyDto>
-                    {
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2,1,3,5,7,8", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7), Salary = new decimal(3500.76)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                        new DummyDto{Id = 1,Name = "Name 1", BirthDate = new DateTime(1987,7,7)},
-                        new DummyDto{Id = 2,Name = "2", BirthDate = new DateTime(1987,7,7),NullableIntegerValue = 135},
-                        new DummyDto{Id = 3,Name = "Name 3", BirthDate = new DateTime(1987,7,7),NullableDateTimeValue = new DateTime(2013,9,18)},
-                    };
+                var dtos = new List<DummyDto>();
+
+                while (dtos.Count < 50)
+                {
+                    dtos.Add(DummyDtoFactory.NewDummyDto());
+                }
+
+                return dtos;
             }
         }
 
@@ -156,5 +139,53 @@ namespace Vtex.Practices.DataTransformation.Tests
         public decimal? Salary { get; set; }
         public int? NullableIntegerValue { get; set; }
         public DateTime? NullableDateTimeValue { get; set; }
+    }
+
+    public static class DummyDtoFactory
+    {
+        public static DummyDto NewDummyDto()
+        {
+            return new DummyDto
+                {
+                    Id = GetRandom(0, 100),
+                    Name = GetRamdomName(),
+                    BirthDate = GetRandomDateTime(),
+                    Salary = new decimal(GetRandom(4000, 10000) + GetRandom(100, 1000) * .1),
+                    NullableIntegerValue = new int?[] { GetRandom(), null, null }[GetRandom(0, 2)],
+                    NullableDateTimeValue = new DateTime?[] { GetRandomDateTime(), null, null }[GetRandom(0, 2)],
+                };
+        }
+
+        private static string GetRamdomName()
+        {
+            return string.Format("{0} {1}",
+                            new[] { "John", "Jack", "Juca", "Chico", "Zé" }[GetRandom()],
+                            new[] { "Doe", "Ellis", "The Ripper", "Roberto", "Johnson" }[GetRandom()]);
+        }
+
+        private static Random GetRandomizer()
+        {
+            var newGuid = Guid.NewGuid();
+            var guidNumber = Regex.Replace(newGuid.ToString(), @"[^\d]", "");
+            var rnd0 = new Random(Convert.ToInt32(guidNumber.Substring(0, 8)));
+            var rnd = new Random(rnd0.Next(5000));
+            return rnd;
+        }
+
+        private static DateTime GetRandomDateTime()
+        {
+            var year = GetRandom(1960, 1990);
+            var month = GetRandom(1, 12);
+            var day = GetRandom(1, month == 2 ? 29 : 30);
+            var randomDateTime = new DateTime(year, month, day);
+
+            return randomDateTime;
+        }
+
+        private static int GetRandom(int min = 0, int max = 4)
+        {
+            var rnd = GetRandomizer();
+            return rnd.Next(min, max);
+        }
     }
 }
